1722d68b042bfc4f5749ae62f082d450a11d2c27
Brian Abdo <abdobrian@gmail.com>
2020-10-29 17:31:29 -0400

    In class work for 10/27

--- ProgrammingFundementals/intitialtest.js
+++ ProgrammingFundementals/intitialtest.js
@@ -1,1 +1,50 @@
+//I'm trying to test something
 console.log("Hello World")
+const firstSecondThirdFourth = "Test String"
+var first = "Second String"
+
+console.log(first)
+
+var numberTest = 2 + 3;
+
+
+
+
+console.log(firstSecondThirdFourth + numberTest +  first)
+
+
+
+function ChooseFootballTeam(City, State){
+
+}
+
+//Kayci Create Function - 
+function birthstone(yearMade, type, colorientation, size){
+    
+    
+    //if year is not a number / this function can't be run
+    
+}
+
+birthstone(1923, "opal", "green", 3, 4)
+function ThisIsOurFirstFunction(name){
+    console.log(name + "Function")
+
+}
+ThisIsOurFirstFunction("first");
+ThisIsOurFirstFunction("second");
+ThisIsOurFirstFunction(first);
+ThisIsOurFirstFunction();
+
+
+function marinaraSauce (cookName, measurment1 = 2) {
+    console.log("Hey, " + cookName + " this is my marinara sauce recipe!" + "Salt Needed: "+ isSaltRequired);
+    console.log("Step 1");
+    console.log("Step 2");
+}
+marinaraSauce ("Jeff");
+marinaraSauce ("Ally");
+
+marinaraSauce ("Sam");
+
+marinaraSauce ("Adam", isSaltRequired = true);
